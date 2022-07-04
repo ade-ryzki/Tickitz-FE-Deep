56150f9d13247dfb85e9ea9101e6fb59d4bbc16c
ade-ryzki <ade.ryzki@gmail.com>
2022-07-05 04:25:29 +0700

    modified table admin and link to

--- src/Page/Admin/Component/Sidebar.js
+++ src/Page/Admin/Component/Sidebar.js
@@ -6,19 +6,19 @@ export const Sidebar = () => {
         <div className='container'>
             <div className='row'>
                 <div className='col-lg-12 d-none d-lg-block'>
-                    <div className='card shadow'>
+                    <div className='card shadow card-2'>
                         <div className='card-body align-self-center text-center my-3'>
-                            <h3 className='fw-bold mb-5'>ADMIN AREA</h3>
-                            <Link to={'/admin'} className='text-black text-decoration-none'>Dashboard</Link>
-                            <hr />
+                            <h3 className='fw-bold mb-5'>ADMIN</h3>
+                            {/* <Link to={'/admin'} className='text-black text-decoration-none'>Dashboard</Link>
+                            <hr /> */}
                             <Link to={'/admin/movie'} className='text-black text-decoration-none'>Movie</Link>
                             <hr />
-                            <Link to={'/admin/cinema'} className='text-black text-decoration-none'>Cinema</Link>
+                            {/* <Link to={'/admin/cinema'} className='text-black text-decoration-none'>Cinema</Link>
                             <hr />
                             <Link to={'/admin/users'} className='text-black text-decoration-none'>Users</Link>
                             <hr />
                             <Link to={'/admin/setting'} className='text-black text-decoration-none'>Setting</Link>
-                            <hr />
+                            <hr /> */}
                             <Link to={'/'} className='text-black text-decoration-none'>Log out</Link>
                         </div>
                     </div>
